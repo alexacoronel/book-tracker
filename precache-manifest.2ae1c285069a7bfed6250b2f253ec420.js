887f7bbead7bb22950f00e6efa299f6f1479ab3a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-07 19:37:24 +0000

    Deploy zero-to-mastery/book-tracker to github.com/zero-to-mastery/book-tracker.git:gh-pages

---
+++ precache-manifest.2ae1c285069a7bfed6250b2f253ec420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c996968eba1b4b374cd1b8ba232d4284",
+    "url": "/book-tracker/index.html"
+  },
+  {
+    "revision": "c1864443091c4d2b8b47",
+    "url": "/book-tracker/static/css/2.514762e1.chunk.css"
+  },
+  {
+    "revision": "5144ef22679c25cbe3f0",
+    "url": "/book-tracker/static/css/main.f0bf30bc.chunk.css"
+  },
+  {
+    "revision": "c1864443091c4d2b8b47",
+    "url": "/book-tracker/static/js/2.21212bae.chunk.js"
+  },
+  {
+    "revision": "5144ef22679c25cbe3f0",
+    "url": "/book-tracker/static/js/main.f4acb6ee.chunk.js"
+  },
+  {
+    "revision": "b7f89f697b84136174f8",
+    "url": "/book-tracker/static/js/runtime~main.1bc555b3.js"
+  }
+]);
